0e51f24ef96f370e6c43f31582dceaee0bbfdc3e
iacobcl <iacob.claudia@gmail.com>
2024-03-04 13:41:37 +0000

    Update mathmanagertest.py

--- mathmanagertest.py
+++ mathmanagertest.py
@@ -5,3 +5,11 @@ class mathmanagertest(unittest.TestCase):
 	def testAdd(self):
 		math = mathmanager()
 		self.assertEqual(math.add(0, 3), 3)
+
+	def testSubtract(self):
+		math = mathmanager()
+		self.assertEqual(math.subtract(0, 3), -3)
+
+	def testMultiply(self):
+		math = mathmanager()
+		self.assertEqual(math.multiply(0, 3), 0)
