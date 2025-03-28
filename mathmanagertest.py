dd2581f6b9638a2a50e1be21f6e4f502193ee636
up2194063 <153841272+up2194063@users.noreply.github.com>
2025-03-28 10:29:37 +0000

    Added test functions for income tax and degree classification

--- mathmanagertest.py
+++ mathmanagertest.py
@@ -13,3 +13,24 @@ class mathmanagertest(unittest.TestCase):
 	def testMultiply(self):
 		math = mathmanager()
 		self.assertEqual(math.multiply(0, 3), 0)
+
+	def test_income_tax_calculator(self):
+		math = mathmanager()
+		self.assertEqual(math.income_tax_calculator(12570), 0)
+		self.assertEqual(math.income_tax_calculator(12571), 2514.2)
+		self.assertEqual(math.income_tax_calculator(50270), 10054)
+		self.assertEqual(math.income_tax_calculator(50271), 20108.4)
+		self.assertEqual(math.income_tax_calculator(50270), 10054)
+		self.assertEqual(math.income_tax_calculator(125140), 50056)
+		self.assertEqual(math.income_tax_calculator(125141), 56313.45)
+
+	def test_income_tax_calculator(self):
+		math = mathmanager()
+		self.assertEqual(math.degree_classification(39, 39, 39, 39), "Fail")
+		self.assertEqual(math.degree_classification(40, 40, 40, 40), "3rd")
+		self.assertEqual(math.degree_classification(50, 50, 50, 50), "2:2")
+		self.assertEqual(math.degree_classification(60, 60, 60, 60), "2:1")
+		self.assertEqual(math.degree_classification(70, 70, 70, 70), "1st")
+
+
+
