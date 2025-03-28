7081f77d1601847a218940a857d6e1dcf2d7a111
up2194063 <153841272+up2194063@users.noreply.github.com>
2025-03-28 10:29:28 +0000

    Added functions for income tax and degree classification

--- mathmanager.py
+++ mathmanager.py
@@ -8,3 +8,27 @@ class mathmanager:
 
 	def multiply(self, a, b):
 			return a*b
+
+	def income_tax_calculator(self, income):
+		if income <= 12570:
+			tax_multiplier = 0
+		elif income <= 50270:
+			tax_multiplier = 0.2
+		elif income <= 125140:
+			tax_multiplier = 0.4
+		else:
+			tax_multiplier = 0.45
+		return tax_multiplier * income
+	
+	def degree_classification(self, m1, m2, m3, m4):
+		avg = (m1 + m2 + m3 + m4) / 4
+		if avg < 40:
+			return "Fail"
+		elif avg < 50:
+			return "3rd"
+		elif avg < 60:
+			return "2:2"
+		elif avg < 70:
+			return "2:1"
+		else:
+			return "1st"
